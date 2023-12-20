cab3ac2334ad6335850e9ddd3aa960fb2a0e2f5b
Kinga <kinga.sabat@gmail.com>
2023-12-21 00:53:18 +0100

    initial commit + redux-toolkit + scss + tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './styles/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='text-xl font-bold underline'>Test</header>
+    </div>
+  );
+}
+
+export default App;
