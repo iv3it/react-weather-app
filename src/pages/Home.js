916eda05a8349bace8758bfc590544cfaca30155
Kinga <kinga.sabat@gmail.com>
2024-01-22 17:26:35 +0100

    add header app logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,6 +27,10 @@ const Home = () => {
   return (
     <div className="container mx-auto px-4 py-4">
       <div className='max-w-screen-lg mx-auto'>
+        <header className="flex items-center mb-6">
+          <img src={`/icons/weather-icons/01d.png`} alt="weather app icon" className='h-5 w-5' />
+          <h1 className='text-white text-xs font-bold ml-2'>Weather app</h1>
+        </header>
         <SearchBar />
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-6">
             <div className="col-span-1 lg:col-span-2">
